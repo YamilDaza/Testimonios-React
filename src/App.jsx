881057949963b830cf165af7bea4fae1ae282b65
Yamil <dazayamil07@gmail.com>
2022-07-23 10:09:44 -0300

    testimonios

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ const img = require.context('./img', true);
 const App = () => {
     return (
         <div className='app'>
-            <h1>Porque estudiar Programacion:</h1>
             <div className='contenedor-principal'>
+                <h1>Porque estudiar programación:</h1>
                 <Testimonio 
                     imagen={img('./butsOne.png')}
                     nombre='Jon Skeet'
@@ -19,13 +19,13 @@ const App = () => {
                     imagen={img('./butsTwo.png')}
                     nombre='Sanjay Ghemawat'
                     cargo='¿Qué le llevó a la fama?: Arquitecto clave de Google'
-                    texto='Credenciales: Ayudó a diseñar e implementar algunos de los sistemas distribuidos más grandes de Google, incluyendo MapReduce, BigTable, Spanner y Google File System. Creó ical, el sistema de calendario. Fue elegido para el National Academy of Engineering en 2009. Ganador del premio ACM-Infosys Foundation Award in the Computing Sciences en 2012.'
+                    texto='Credenciales: Ayudó a diseñar e implementar algunos de los sistemas distribuidos más grandes de Google, incluyendo MapReduce, BigTable, Spanner y Google File System. Creó ical, el sistema de calendario. Fue elegido para el National Academy of Engineering en 2009.'
                 />
                 <Testimonio 
                     imagen={img('./butsThree.png')}
                     nombre='Fabrice Bellard'
                     cargo='¿Qué le llevó a la fama?: creador de QEMU'
-                    texto='Credenciales: Creó una variedad de programas de código abierto, incluyendo QEMU, una platforma para la emulación de hardware emulation y virtualización, FFmpeg, para el manejo de datos multimedia, el Tiny C Compiler y LZEXE, un ejecutable para comprimir archivos. Ganador del  Obfuscated C Code Contest en 2000 y 2001 y el ganador del Google-O Reilly Open Source Award en 2011. Antiguo plus-marquista mundial en el cálculo del máximo número de dígitos en Pi.'
+                    texto='Credenciales: Creó una variedad de programas de código abierto, incluyendo QEMU, una platforma para la emulación de hardware emulation y virtualización, FFmpeg, para el manejo de datos multimedia, el Tiny C Compiler y LZEXE, un ejecutable para comprimir archivos.'
                 />
             </div>
         </div>
